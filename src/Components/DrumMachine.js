7a8409d6411529edbfa4cf9a9c6deab298c4c2da
DanGrund <hello@dangrund.com>
2017-02-22 10:00:01 -0700

    add links to all components

--- src/Components/DrumMachine.js
+++ src/Components/DrumMachine.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import DrumRack from './DrumRack'
+import { Link } from 'react-router';
 
 export default class DrumMachine extends React.Component {
   render(){
     return (
       <div id='drum-machine-container'>
+
         <div id='play-controls'>
           <div>play</div>
           <div>stop</div>
-          <div>visuals</div>
+          <Link to={'/visualizer'}>
+            <button>visualzzz</button>
+          </Link>
+          <Link to={'/instructions'}>
+            <button>I need more instruction</button>
+          </Link>
         </div>
+
         <div id='drum-racks'>
           <DrumRack />
           <DrumRack />
           <DrumRack />
           <DrumRack />
         </div>
+        
       </div>
     )
   }
